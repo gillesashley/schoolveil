9f538f88ac17aff5cb45c363eb7594e7c5702641
gillesashley <xylaray37@gmail.com>
2023-03-30 11:19:29 +0000

    Initial commit

---
+++ resources/views/layouts/inc/sidenav.blade.php
@@ -0,0 +1,56 @@
+<!-- [ navigation menu ] start -->
+<nav class="pcoded-navbar">
+    <div class="navbar-wrapper">
+        <div class="navbar-brand header-logo">
+            <a href="index.html" class="b-brand">
+                <div class="b-bg">
+                    <i class="feather icon-trending-up"></i>
+                </div>
+                <span class="b-title">Datta Able</span>
+            </a>
+            <a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
+        </div>
+        <div class="navbar-content scroll-div">
+            <ul class="nav pcoded-inner-navbar">
+                <li class="nav-item pcoded-menu-caption">
+                    <label>Navigation</label>
+                </li>
+                <li data-username="dashboard"
+                    class="nav-item pcoded-hasmenu">
+                    <a href="{{route('home')}}" class="nav-link"><span class="pcoded-micon"><i
+                                class="feather icon-home"></i></span><span class="pcoded-mtext">Dashboard</span></a>
+                </li>
+                <li data-username="students"
+                    class="nav-item pcoded-hasmenu">
+                    <a href="{{route('students.index')}}" class="nav-link"><span class="pcoded-micon"><i
+                                class="feather icon-layout"></i></span><span class="pcoded-mtext">Students</span></a>
+                </li>
+                <li data-username="widget Statistic Data Table User card Chart" class="nav-item pcoded-hasmenu">
+                    <a href="#!" class="nav-link"><span class="pcoded-micon"><i
+                                class="feather icon-layers"></i></span><span class="pcoded-mtext">Grading</span><span
+                            class="pcoded-badge label label-info">100+</span></a>
+                    <ul class="pcoded-submenu">
+                        <li class=""><a href="widget-statistic.html" class="">Statistic</a></li>
+                        <li class=""><a href="widget-data.html" class="">Data</a></li>
+                        <li class=""><a href="widget-table.html" class="">Table</a></li>
+                        <li class=""><a href="widget-user-card.html" class="">User</a></li>
+                        <li class=""><a href="widget-chart.html" class="">Chart</a></li>
+                    </ul>
+                </li>
+                <li data-username="widget Statistic Data Table User card Chart" class="nav-item pcoded-hasmenu">
+                    <a href="#!" class="nav-link"><span class="pcoded-micon"><i
+                                class="feather icon-layers"></i></span><span class="pcoded-mtext">Reports</span><span
+                            class="pcoded-badge label label-info">100+</span></a>
+                    <ul class="pcoded-submenu">
+                        <li class=""><a href="widget-statistic.html" class="">Statistic</a></li>
+                        <li class=""><a href="widget-data.html" class="">Data</a></li>
+                        <li class=""><a href="widget-table.html" class="">Table</a></li>
+                        <li class=""><a href="widget-user-card.html" class="">User</a></li>
+                        <li class=""><a href="widget-chart.html" class="">Chart</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- [ navigation menu ] end -->
---
+++ resources/views/pages/students/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- [ Main Content ] start -->
+    <div class="pcoded-main-container">
+        <div class="pcoded-wrapper">
+            <div class="pcoded-content">
+                <div class="pcoded-inner-content">
+                    <!-- [ breadcrumb ] start -->
+
+                    <!-- [ breadcrumb ] end -->
+                    <div class="main-body">
+                        <div class="page-wrapper">
+                            <!-- [ Main Content ] start -->
+                            <div class="row">
+                                <h1>Students</h1>
+                            </div>
+                            <!-- [ Main Content ] end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- [ Main Content ] end -->
+@endsection
